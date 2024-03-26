fc20c2cb81a9eebfd2f8b76360c36872be4cd80c
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-03-26 18:36:30 +0300

    Insert a document in Python

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+a Python function that inserts a new document in a collection based on kwargs:
+args:
+    mongo_collection: collection
+    **kwargs: additional arguments
+return:
+    new_id
+"""
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """a function that inserts a new document
+    in a collection based on kwargs
+    """
+    if mongo_collection is None:
+        """return empty set if mongo_collection is empty"""    
+        return []
+    if kwargs is None:
+        return None
+    newResuilt = mongo_collection.insert({kwargs})
+    return newResuilt.inserted_id
+    
+    
